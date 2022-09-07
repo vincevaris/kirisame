f6d4e6767425ec01ce5e36aa087fe562568d9682
jamalvw <jvwill00@gmail.com>
2022-09-07 00:08:08 -0400

    Use .env in deploy-commands and register globally

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const fs = require('node:fs');
 const path = require('node:path');
 const { REST } = require('@discordjs/rest');
@@ -13,8 +14,8 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
-rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
+rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
 	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
 	.catch(console.error);
